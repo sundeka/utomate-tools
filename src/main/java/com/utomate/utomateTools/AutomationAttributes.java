fc733b76d3d3879120d4d9cc14e538f1133ebcc2
Kalle Sundell <sundellkalle@gmail.com>
2024-10-09 15:10:00 +0300

    init `AutomationAttributes` class

--- src/main/java/com/utomate/utomateTools/AutomationAttributes.java
+++ src/main/java/com/utomate/utomateTools/AutomationAttributes.java
@@ -1,16 +1,7 @@
 package com.utomate.utomateTools;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AutomationAttributes {
 	private String name;
-
-	public Map<String, Object> getJson() {
-		Map<String, Object> m = new HashMap<String, Object>();
-		m.put("name", this.getName());
-		return m;
-	}
 	
 	public String getName() {
 		return name;
