112c5f6b535cab9ec572edf8d269d0bf527a7e27
Kalle Sundell <sundellkalle@gmail.com>
2024-10-11 12:34:02 +0300

    generate ZIP instead of JAR

--- src/main/java/com/utomate/utomateTools/FileController.java
+++ src/main/java/com/utomate/utomateTools/FileController.java
@@ -3,7 +3,6 @@ package com.utomate.utomateTools;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
@@ -22,25 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class FileController {
 	
 	private static final String path = "src/main/resources/files/";
-	private static final String fileFormat = ".jar";
-	
-	private void createFile(String file, AutomationAttributes attributes) throws IOException {
-		System.out.println("Creating the following file: " + file.toString());
-		JavaFactory java = new JavaFactory();
-		FileWriter writer = new FileWriter(file);
-		writer.write(java.getCode());
-		writer.close();
-		System.out.println("File created!");
-	}
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/generate")
 	public String generateFile(@RequestBody AutomationAttributes attributes) {
+		// Create folder at src/main/resources/files/<uuid>
 		String uuid = UUID.randomUUID().toString();
-		new File(path + uuid).mkdirs();
-		String file = path + uuid + "/" + attributes.getName() + fileFormat;
+		String directory = path + uuid;
+		new File(directory).mkdirs();
+		System.out.println("Folder created at: " + directory);
+		
+		// Create files
+		JavaFactory java = new JavaFactory(directory);	
 		try {
-			createFile(file, attributes);
+			java.createProject(attributes.getName());
+			java.generateInstructions(attributes.getName());
+			java.writeCode(attributes);
+			System.out.println("Project created! Converting to .ZIP...");
+			java.createZip(attributes.getName());
+			System.out.println("ZIP conversion successful!");
 		} catch (IOException e) {
 			throw new FileException(e.getMessage());
 		}
@@ -59,7 +58,7 @@ public class FileController {
 	        byte[] byteArray = IOUtils.toByteArray(in);
 	        return byteArray;
 	    } catch (FileNotFoundException e) {
-	        throw new FileException(id);
+	        throw new FileException(e.getMessage());
 	    } catch (IOException e) {
 	        throw new IOException("Error reading file: " + files[0].getAbsolutePath(), e);
 	    }
