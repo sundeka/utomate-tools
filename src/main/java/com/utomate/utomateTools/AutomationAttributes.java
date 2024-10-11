12e2570f5d721e9d8f2f96cf3c5b1e044cf82c5c
Kalle Sundell <sundellkalle@gmail.com>
2024-10-11 15:28:06 +0300

    convert (some) regular datatype inputs to newly created structs

--- src/main/java/com/utomate/utomateTools/AutomationAttributes.java
+++ src/main/java/com/utomate/utomateTools/AutomationAttributes.java
@@ -1,29 +1,42 @@
 package com.utomate.utomateTools;
 
+import java.util.ArrayList;
+
+import com.utomate.utomateTools.structs.Driver;
+import com.utomate.utomateTools.structs.Step;
+import com.utomate.utomateTools.structs.StepType;
 
 public class AutomationAttributes {
 	private String name;
 	private boolean headless;
-	private String webDriver;
+	private Driver webdriver;
+	private ArrayList<Step> steps;
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	// Default getters & setters
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	public boolean isHeadless() { return headless; }
+	public void setHeadless(boolean headless) { this.headless = headless; }
+	public Driver getWebdriver() { return webdriver; }
+	public void setWebdriver(Driver webdriver) { this.webdriver = webdriver; }
+	public ArrayList<Step> getSteps() { return steps; }
+	public void setSteps(ArrayList<Step> steps) { this.steps = steps; }
 	
-	public boolean isHeadless() {
-		return headless;
-	}
-	public void setHeadless(boolean headless) {
-		this.headless = headless;
-	}
-	
-	public String getWebDriver() {
-		return webDriver;
-	}
-	public void setWebDriver(String webDriver) {
-		this.webDriver = webDriver;
+	// Custom methods
+	public ArrayList<StepType> getAllUniqueStepTypes() {
+		ArrayList<StepType> uniques = new ArrayList<>();
+		for (Step step : steps) {
+			boolean found = false;
+			for (StepType u : uniques) {
+				if (u.equals(step.type)) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				uniques.add(step.type);
+			}
+		}
+		return uniques;
 	}
 }
